77ca375b9edd81673110ebbf00a9ca98e94bde07
tarnishablec <tarnishablec@outlook.com>
2024-12-01 18:05:49 +0800

    UE 5.5 Compatibility

--- Source/MDMetaDataEditor/Private/Types/MDMetaDataEditorPropertyType.h
+++ Source/MDMetaDataEditor/Private/Types/MDMetaDataEditorPropertyType.h
@@ -3,7 +3,11 @@
 #pragma once
 
 #include "EdGraph/EdGraphPin.h"
+#if ENGINE_MAJOR_VERSION >= 5 && ENGINE_MINOR_VERSION >= 5
+#include "StructUtils/InstancedStruct.h"
+#else
 #include "InstancedStruct.h"
+#endif
 
 #include "MDMetaDataEditorPropertyType.generated.h"
 
@@ -13,84 +17,95 @@ struct FEdGraphTerminalType;
 UENUM()
 enum class EMDMetaDataPropertyContainerType : uint8
 {
-	None,
-	Array,
-	Set,
-	Map
+    None,
+    Array,
+    Set,
+    Map
 };
 
 USTRUCT()
 struct FMDMetaDataEditorPropertyType
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	FMDMetaDataEditorPropertyType(
-		FName PropertyType = NAME_None,
-		FName PropertySubType = NAME_None,
-		TSoftObjectPtr<UObject> PropertySubTypeObject = nullptr,
-		FSimpleMemberReference PropertySubTypeMemberReference = FSimpleMemberReference(),
-		FInstancedStruct ValueType = FInstancedStruct(),
-		EMDMetaDataPropertyContainerType ContainerType = EMDMetaDataPropertyContainerType::None
-		)
-		: PropertyType(PropertyType)
-		, PropertySubType(PropertySubType)
-		, PropertySubTypeObject(PropertySubTypeObject)
-		, PropertySubTypeMemberReference(PropertySubTypeMemberReference)
-		, ValueType(ValueType)
-		, ContainerType(ContainerType)
-	{
-		// Fix any newly constructed property types.
-		FixUp();
-	}
+    FMDMetaDataEditorPropertyType(
+        FName PropertyType = NAME_None,
+        FName PropertySubType = NAME_None,
+        TSoftObjectPtr<UObject> PropertySubTypeObject = nullptr,
+        FSimpleMemberReference PropertySubTypeMemberReference = FSimpleMemberReference(),
+        FInstancedStruct ValueType = FInstancedStruct(),
+        EMDMetaDataPropertyContainerType ContainerType = EMDMetaDataPropertyContainerType::None
+    )
+        : PropertyType(PropertyType)
+          , PropertySubType(PropertySubType)
+          , PropertySubTypeObject(PropertySubTypeObject)
+          , PropertySubTypeMemberReference(PropertySubTypeMemberReference)
+          , ValueType(ValueType)
+          , ContainerType(ContainerType)
+    {
+        // Fix any newly constructed property types.
+        FixUp();
+    }
 
-	FEdGraphPinType ToGraphPinType() const;
-	FEdGraphTerminalType ToGraphTerminalType() const;
+    FEdGraphPinType ToGraphPinType() const;
+    FEdGraphTerminalType ToGraphTerminalType() const;
 
-	void SetFromGraphPinType(const FEdGraphPinType& GraphPinType);
-	void SetFromGraphTerminalType(const FEdGraphTerminalType& GraphTerminalType);
+    void SetFromGraphPinType(const FEdGraphPinType& GraphPinType);
+    void SetFromGraphTerminalType(const FEdGraphTerminalType& GraphTerminalType);
 
-	bool DoesMatchProperty(const FProperty* Property) const;
+    bool DoesMatchProperty(const FProperty* Property) const;
 
-	UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
-	FName PropertyType = NAME_None;
+    UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
+    FName PropertyType = NAME_None;
 
-	UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
-	FName PropertySubType = NAME_None;
+    UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
+    FName PropertySubType = NAME_None;
 
-	UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
-	TSoftObjectPtr<UObject> PropertySubTypeObject;
+    UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
+    TSoftObjectPtr<UObject> PropertySubTypeObject;
 
-	UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
-	FSimpleMemberReference PropertySubTypeMemberReference;
+    UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
+    FSimpleMemberReference PropertySubTypeMemberReference;
 
-	UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
-	FInstancedStruct ValueType;
-	FMDMetaDataEditorPropertyType& SetValueType(FMDMetaDataEditorPropertyType&& InValueType) { ValueType.InitializeAs<FMDMetaDataEditorPropertyType>(InValueType); return *this; }
+    UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
+    FInstancedStruct ValueType;
 
-	UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
-	EMDMetaDataPropertyContainerType ContainerType = EMDMetaDataPropertyContainerType::None;
-	FMDMetaDataEditorPropertyType& SetContainerType(EMDMetaDataPropertyContainerType InContainerType) { ContainerType = InContainerType; return *this; }
+    FMDMetaDataEditorPropertyType& SetValueType(FMDMetaDataEditorPropertyType&& InValueType)
+    {
+        ValueType.InitializeAs<FMDMetaDataEditorPropertyType>(InValueType);
+        return *this;
+    }
 
-	void FixUp();
+    UPROPERTY(EditAnywhere, Config, Category = "Meta Data Editor")
+    EMDMetaDataPropertyContainerType ContainerType = EMDMetaDataPropertyContainerType::None;
 
-	bool operator==(const FMDMetaDataEditorPropertyType& Other) const;
-	bool operator!=(const FMDMetaDataEditorPropertyType& Other) const
-	{
-		return !(*this == Other);
-	}
+    FMDMetaDataEditorPropertyType& SetContainerType(EMDMetaDataPropertyContainerType InContainerType)
+    {
+        ContainerType = InContainerType;
+        return *this;
+    }
 
-	friend uint32 GetTypeHash(const FMDMetaDataEditorPropertyType& Key)
-	{
-		uint32 Hash = GetTypeHash(Key.PropertyType);
-		Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubType));
-		Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubTypeObject));
-		Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubTypeMemberReference.MemberParent));
-		Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubTypeMemberReference.MemberName));
-		Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubTypeMemberReference.MemberGuid));
-		Hash = HashCombine(Hash, GetTypeHash(Key.ValueType.GetScriptStruct()));
-		Hash = HashCombine(Hash, GetTypeHash(Key.ValueType.GetMemory()));
-		Hash = HashCombine(Hash, GetTypeHash(Key.ContainerType));
-		return Hash;
-	}
+    void FixUp();
+
+    bool operator==(const FMDMetaDataEditorPropertyType& Other) const;
+
+    bool operator!=(const FMDMetaDataEditorPropertyType& Other) const
+    {
+        return !(*this == Other);
+    }
+
+    friend uint32 GetTypeHash(const FMDMetaDataEditorPropertyType& Key)
+    {
+        uint32 Hash = GetTypeHash(Key.PropertyType);
+        Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubType));
+        Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubTypeObject));
+        Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubTypeMemberReference.MemberParent));
+        Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubTypeMemberReference.MemberName));
+        Hash = HashCombine(Hash, GetTypeHash(Key.PropertySubTypeMemberReference.MemberGuid));
+        Hash = HashCombine(Hash, GetTypeHash(Key.ValueType.GetScriptStruct()));
+        Hash = HashCombine(Hash, GetTypeHash(Key.ValueType.GetMemory()));
+        Hash = HashCombine(Hash, GetTypeHash(Key.ContainerType));
+        return Hash;
+    }
 };
